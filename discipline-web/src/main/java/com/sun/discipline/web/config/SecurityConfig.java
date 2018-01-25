342cd37b1065d60afbbb3d3c75a59d6507c87844
theOneSun <sunjian_better@163.com>
2018-01-25 18:38:24 +0800

    增加安全配置

---
+++ discipline-web/src/main/java/com/sun/discipline/web/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.sun.discipline.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author sunjian.
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+
+    }
+}
